a560d84fdefa01d9593eddd12b8c613870fcef11
Fadhl Ba wazir <fadhl.bawazir@gmail.com>
2018-06-17 17:07:44 +0300

    uprading laravel to 5.3

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -6,6 +6,30 @@ use Closure;
 use Illuminate\Support\Facades\Auth;
 
 class Admin
+// the one I made with thr instructor - un-comment this to make it work
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  \Closure  $next
+//      * @return mixed
+//      */
+//     public function handle($request, Closure $next)
+//     {
+//         if(Auth::check()){
+
+//             if(Auth::user()->isAdmin()){
+//                 return $next($request);
+//             }
+
+//         }
+
+//         return redirect('/');
+//     }
+// }
+
+// this one is working like the above but without the function isAdmin() coz I wrote the function instead in Middleware itself
 {
     /**
      * Handle an incoming request.
@@ -18,7 +42,7 @@ class Admin
     {
         if(Auth::check()){
 
-            if(Auth::user()->isAdmin()){
+            if(Auth::user()->role->name == 'administrator' && Auth::user()->is_active == 1){
                 return $next($request);
             }
 
